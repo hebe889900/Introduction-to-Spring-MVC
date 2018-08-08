fd452e6b15868b25af5ad12ef6cf27543ae3361b
Yan <jackyan63@gmail.com>
2018-08-08 14:44:23 -0400

    Add chain controller

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -12,6 +12,12 @@ public class MinutesController {
 	@RequestMapping(value = "addMinutes")
 	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise) {
 		System.out.println("exercise: " + exercise.getMinutes());
-		return "addMinutes";
+		return "forward:addMoreMinutes.html";
 	}
+	
+	@RequestMapping(value = "addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise) {
+		System.out.println("exercising: " + exercise.getMinutes());
+		return "addMinutes";
+	}	
 }
