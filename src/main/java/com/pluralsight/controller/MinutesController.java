a5737debf3faa031f9ecfe0cb70c8c7f0b20e57a
Yan <jackyan63@gmail.com>
2018-08-08 14:56:35 -0400

    Added JSP

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -12,12 +12,13 @@ public class MinutesController {
 	@RequestMapping(value = "addMinutes")
 	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise) {
 		System.out.println("exercise: " + exercise.getMinutes());
-		return "forward:addMoreMinutes.html";
+		return "addMinutes";
 	}
 	
-	@RequestMapping(value = "addMoreMinutes")
-	public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise) {
-		System.out.println("exercising: " + exercise.getMinutes());
-		return "addMinutes";
-	}	
+//	@RequestMapping(value = "addMoreMinutes")
+//	public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise) {
+//		System.out.println("exercising: " + exercise.getMinutes());
+//		return "addMinutes";
+//	}	
+//
 }
